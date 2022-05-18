d323787cdbb9cb7911d7cf0547dedb472d9ec8b2
T <41618400+carcinogen0513@users.noreply.github.com>
2022-05-18 16:03:59 +0800

    5/13 ファイルを挿入するところまで完了

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
-import datetime
-from datetime import end
-import pandas_datareader.data as web
+import glob
+import pandas as pd
+import os
 
-start = datetime.date(2019, 1, 1)
-end = datetime.date(2020, 1, 1)
 
-df_ntt = web.DataReader('9432.T',"yahoo",start,end)
-df_kddi = web.DataReader('9433.T',"yahoo",start,end)
+file_root = '/Users/tm/PycharmProjects/pythonProject1/data'
+file = glob.glob(file_root+'/*')
+# print('printed:'+file[100])
+with open(file[0] , "r" , encoding='euc_jp') as f:
+     html = f.read()
+df = pd.read_html(html)
+df
+
+
+#
+
+
+# _df=pd.DataFrame()
+# def open(path: object) -> object:
+#      with open(path,"rb",encoding='euc_jp') as f:
+#           html= f.read()
+#     df = pd.read_html(html)
+#     print(df)
+# #
+# open(file[100])
+
+
+#
+# def open(i):
+#      with open(i,"r",encoding='euc_jp') as f:
+#           ok= f.read()
+#
+# def read(i):
+#      df = pd.read_html(data)[0]
+#      _df = pd.concat([df, _df])
+#
+# def mining():
+#      for i in file:
+#           open(i)
+#           try:
+#                read(i)
+#           except ImportError:
+#                os.remove(i)
+#                open(i+1)
+#
+# if __name__=='__main__':
+#      mining()
